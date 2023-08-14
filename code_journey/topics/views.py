0aaacbb55161011145524a8e8498d09b0ad683c5
rajibmitra <rajibmitter@gmail.com>
2023-08-14 21:12:44 +0530

    fixed the homepage

--- code_journey/topics/views.py
+++ code_journey/topics/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Topic
+from django.http import HttpResponse
 
 def index(request):
     root_topic = Topic.objects.get(name="Arrays")
@@ -8,3 +9,6 @@ def index(request):
 def topic_detail(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     return render(request, 'topics/topic_detail.html', {'topic': topic})
+
+def home(request):
+    return HttpResponse("Welcome to my LeetCode journey!")
